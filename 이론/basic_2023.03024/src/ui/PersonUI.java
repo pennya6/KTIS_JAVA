1326746aa99322270fecc5a82dd03d4adcc25b3d
DESKTOP-0KI5EE0\김시연 <pennya6@naverr.comm>
2023-03-24 14:53:00 +0900

    간단한 수정

--- 이론/basic_2023.03024/src/ui/PersonUI.java
+++ 이론/basic_2023.03024/src/ui/PersonUI.java
@@ -5,12 +5,12 @@ public class PersonUI {
         menu();
     }
     public void menu(){
-        System.out.println("===================");
+        System.out.println("==========등록부==========");
         System.out.println("1.사람등록");
         System.out.println("2.사람조회");
         System.out.println("3.사람삭제");
         System.out.println("4.등록부종료");
-        System.out.println("===================");
+        System.out.println("=========================");
     }
 
 }
