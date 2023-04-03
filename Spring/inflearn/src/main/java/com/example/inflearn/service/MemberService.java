f3533886116695418f7a6fbd57c5402ab4fa7da4
pennya6 <pennya6@naver.com>
2023-04-03 17:52:49 +0900

    중복등록불가 성공, 삭제 실패

--- Spring/inflearn/src/main/java/com/example/inflearn/service/MemberService.java
+++ Spring/inflearn/src/main/java/com/example/inflearn/service/MemberService.java
@@ -4,11 +4,26 @@ import com.example.inflearn.domain.Member;
 import com.example.inflearn.repository.MemberRepository;
 import com.example.inflearn.repository.MemoryMemberRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public class MemberService {
     private final MemberRepository memberRepository=new MemoryMemberRepository();
 
     private Long join(Member member){
         validateDuplicateMember(member);
         memberRepository.save(member);
+        return member.getId();
+    }
+    private void validateDuplicateMember(Member member){
+        memberRepository.findByName(member.getName()).ifPresent(m->{
+            throw  new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
     }
 }
--- leeron/03/spring01/src/main/java/com/example/spring01/controller/TodoController.java
+++ leeron/03/spring01/src/main/java/com/example/spring01/controller/TodoController.java
@@ -16,11 +16,11 @@ public class TodoController {
         return todolist; //string값
     }
 
-    @GetMapping("/receiveList")
-    public ArrayList<String> receiveList(String todo){ //todo라는 이름으로 들어옴, 밖에서 들어오는 구멍
-        todolist.add(todo);
-        return todolist; //string값
-    }
+//    @GetMapping("/receiveList")
+//    public ArrayList<String> receiveList(String todo){ //todo라는 이름으로 들어옴, 밖에서 들어오는 구멍
+//        todolist.add(todo);
+//        return todolist; //string값
+//    }
 
     @GetMapping("/deleteList")
     public ArrayList<String> deleteList(int todo){ //todo라는 이름으로 들어옴, 밖에서 들어오는 구멍
@@ -28,5 +28,36 @@ public class TodoController {
         return todolist; //string값
     }
 
+    @GetMapping("/receiveList")
+    public ArrayList<String> receiveList(String todo){ //todo라는 이름으로 들어옴, 밖에서 들어오는 구멍
+        //중복확인
+        boolean check=checkList(todo);
+        if (check==false){
+            todolist.add(todo);
+        }
+        return todolist; //string값
+    }
+
+    public boolean checkList(String todo){
+        boolean isDuplicated=false; //중복아님
+
+        for(int i=0;i<todolist.size();i++){
+            if(todolist.get(i).equals(todo)){
+                isDuplicated=true;
+            }
+        }
+        return isDuplicated;
+    }
+
+    //단어 입력시 단어가 있는 거 삭제
+    @GetMapping("/deleteword")
+    public ArrayList<String> deleteWord(String todo){ //todo라는 이름으로 들어옴, 밖에서 들어오는 구멍
+        boolean check=checkList(todo); //중복확인
+        if (check==true) {
+            todolist.remove(todo);
+            System.out.println("delete");
+        }
+        return todolist; //string값
+    }
 
 }
