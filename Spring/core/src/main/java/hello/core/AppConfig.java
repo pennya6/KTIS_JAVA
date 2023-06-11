cc05baeb0cd4ad8e2c1f9bcfe12be41b319ff5a4
김시연 <pennya6@hs.ac.kr>
2023-06-11 19:01:43 +0900

    session 3 finish

--- Spring/core/src/main/java/hello/core/AppConfig.java
+++ Spring/core/src/main/java/hello/core/AppConfig.java
@@ -9,22 +9,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+//Appconfig에 설정을 구성한다는 뜻
+@Configuration
 public class AppConfig {
+    //스프링 컨테이너에 스프링 빈으로 등록
+    @Bean
     public MemberService memberService(){
         //MemberServiceImpl 생성
         //MemoryMemberRepository 생성
         return new MemberServiceImpl(memberRepository());
     }
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy()
         );
     }
+    @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
+    @Bean
     public DiscountPolicy discountPolicy(){
         //return new FixDiscountPolicy();
         //Appconfig에서 할인 정책 역할 구현을 변경
