3a667d2a9eae692495c473d9ad08e1370825f960
김시연 <pennya6@hs.ac.kr>
2023-06-18 22:05:32 +0900

    appconfig.xml finish

--- Spring/core/src/test/java/hello/core/beanfind/ApplicationContextExtendFindTest.java
+++ Spring/core/src/test/java/hello/core/beanfind/ApplicationContextExtendFindTest.java
@@ -12,6 +12,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
 public class ApplicationContextExtendFindTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
@@ -22,6 +24,37 @@ public class ApplicationContextExtendFindTest {
         //DiscountPolicy bean=ac.getBean(DiscountPolicy.class);
         Assertions.assertThrows(NoUniqueBeanDefinitionException.class,
                 ()->ac.getBean(DiscountPolicy.class));
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 빈 이름을 지정하면 된다")
+    void findBeanByParentTypeBeanName(){
+        DiscountPolicy rateDiscountPolicy=ac.getBean("rateDiscountPolicy",DiscountPolicy.class);
+        org.assertj.core.api.Assertions.assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
+    @Test
+    @DisplayName("특정 하위 타입으로 조회")
+    void findBeanBySubType(){
+        RateDiscountPolicy bean=ac.getBean(RateDiscountPolicy.class);
+        org.assertj.core.api.Assertions.assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
+    }
+    @Test
+    @DisplayName("부모타입으로 모두 조회하기")
+    void findAllBeanByParentType(){
+        Map<String,DiscountPolicy> map=ac.getBeansOfType(DiscountPolicy.class);
+        org.assertj.core.api.Assertions.assertThat(map.size()).isEqualTo(2);
+        for(String key: map.keySet()){
+            System.out.println("key = " + key+" value="+map.get(key)) ;
+        }
+    }
+    @Test
+    @DisplayName("부모타입으로 모두 조회하기 - Object")
+    void findAllBeanByObjectType(){
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value=" +
+                    beansOfType.get(key));
+        }
     }
 
     @Configuration
