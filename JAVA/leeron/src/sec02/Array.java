8a76c3e228a0327259c85f09062bd3cc974db805
pennya6 <pennya6@naver.com>
2023-06-23 17:09:31 +0900

    java 이론 공부 시작

---
+++ JAVA/leeron/src/sec02/Array.java
@@ -0,0 +1,18 @@
+package sec02;
+
+public class Array {
+    public static void main(String[] args){
+        int[] scores={83,90,87};
+        System.out.println(scores[0]);
+        System.out.println(scores[1]);
+        System.out.println(scores[2]);
+
+        int sum=0;
+        for(int i=0;i<3;i++){
+            sum+=scores[i];
+        }
+        double avg=(double)sum/3;
+        System.out.println("avg = " + avg);
+    }
+
+}
