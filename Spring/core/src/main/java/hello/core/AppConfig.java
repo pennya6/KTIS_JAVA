b1a1527d7e2162cf9fcef9ce9f348d4221355764
pennya6 <pennya6@naver.com>
2023-06-26 09:50:00 +0900

    singleton

--- Spring/core/src/main/java/hello/core/AppConfig.java
+++ Spring/core/src/main/java/hello/core/AppConfig.java
@@ -20,10 +20,12 @@ public class AppConfig {
     public MemberService memberService(){
         //MemberServiceImpl 생성
         //MemoryMemberRepository 생성
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy()
@@ -31,6 +33,7 @@ public class AppConfig {
     }
     @Bean
     public MemberRepository memberRepository(){
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
