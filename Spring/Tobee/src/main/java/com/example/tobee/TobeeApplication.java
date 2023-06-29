7c725609420da1863fcb6ac55571a6bf2d5f517a
pennya6 <pennya6@naver.com>
2023-06-29 17:58:54 +0900

    ai

---
+++ Spring/Tobee/src/main/java/com/example/tobee/TobeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tobee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobeeApplication.class, args);
+    }
+
+}
