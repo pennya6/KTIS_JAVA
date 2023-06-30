e2be192a59ce34b2db797ccca9e55f6792128a1a
pennya6 <pennya6@naver.com>
2023-06-30 17:23:49 +0900

    component scan

--- Spring/core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ Spring/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements  MemberRepository{
     private static Map<Long,Member> store=new HashMap<>(); //개발용으로 단순하게 이용
     @Override
--- Spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,7 +3,10 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
 
@@ -15,6 +18,7 @@ public class OrderServiceImpl implements OrderService{
     // 오직 Appconfig에서 결정
     private  final DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
